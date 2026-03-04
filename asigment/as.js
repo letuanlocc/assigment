af7e71eb755757503d147d4bc57a6ce704d8b1a1
Le Tuan Loc <locletuan5@gmail.com>
2026-03-04 08:26:18 +0700

    push data api

--- asigment/as.js
+++ asigment/as.js
@@ -1,35 +1,45 @@
-function fetchData(url,data,callback){
-    fetch(url,
-        {
-            method: 'POST',
-            headers: {'Content-Type':'application/json'},
-            body: JSON.stringify(data)
-        }
-    )
-        .then(response => {
-            if(!response.ok){
-                throw new Error('Lỗi khi thêm dữ liệu vào API');
-            }
-            return response.json();
-        })
-        .then(data => {
-            callback(null,data);
-        })
-        .catch(error => {
-            callback(error,null);
-        });
-}
+const apiUrl = "https://69a77efa2cd1d0552690c46c.mockapi.io/api/assigment/table";
 
-function handle(error,data){
-    if(error){
-        console.error("ERROR",error.message);
-    }else{
-        console.log("Data: ",data);
+// Gửi dữ liệu lên mock
+async function fetchData(url, data) {
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (!response.ok) {
+            throw new Error("Thêm phòng thất bại");
+        }
+
+        const result = await response.json();
+        alert("Thêm phòng thành công!");
+        console.log(result);
+
+    } catch (error) {
+        console.error(error.message);
     }
 }
-const apiUrl = 'https://69a77efa2cd1d0552690c46c.mockapi.io/api/assigment/table';
-const newData = {
-    name: "Tuấn Lộc",
-    link: 'https://example.com/avatar11.jpg'
+
+// Lấy dữ liệu từ form
+function getData() {
+    let name = document.getElementById("name_room").value;
+    let describe = document.getElementById("describe_room").value;
+    let quantity = document.getElementById("quantity").value;
+    let image = document.getElementById("image").value;
+    const newData = {
+        name,
+        describe,
+        quantity,
+        image
+    };
+
+    fetchData(apiUrl, newData);
 }
-fetchData(apiUrl,newData,handle);
+
+// Gắn sự kiện click
+document.getElementById("addRoomBtn")
+        .addEventListener("click", getData);
